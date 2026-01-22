1dc337ed36c2ee603e6ac2c8b47fdd2992924393
r-a-y-y-a <rayson.yaphj@gmail.com>
2026-01-22 16:27:42 +0800

    Level-0

---
+++ src/main/java/Fishball.java
@@ -0,0 +1,15 @@
+public class Duke {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        //System.out.println(logo);
+        System.out.println("Hello, I'm Fishball!\n");
+        System.out.println("What can I do for you?\n" +
+                        "____________________________________________________________\n" +
+        "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+    }
+}
